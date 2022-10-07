1781964917b954be14bad1dce0a62928f4e94604
Jesse Brunner <jesse.brunner@wsu.edu>
2022-10-06 17:47:19 -0700

    Added BirthDeath->lambda & r; small changes to exponential growth and popgrowth models

--- CreatePP_SankeyDiagrams.R
+++ CreatePP_SankeyDiagrams.R
@@ -13,3 +13,24 @@ SankeyR(input = 100, losses = 100*c(0.47, 0.25, 0.19, 0.04, 0.05),
 SankeyR(input = 2500, losses = c(1200, 715, 36, 549),
         labels = c("Available", "not eaten", "defecated", "respired", "production"),
         unit = "ug/day", format = "pdf")
+
+
+## For homework
+# We measured the amount of dissolved oxygen in both bottles as 6.24 mg O2/L right before we set them out, but we came back a day later, the oxygen concentration in the light bottle was 6.39 mg O2/L and that in the dark bottle was 6.16 mg O2/L
+c(6.39, 6.16) - 6.24 # changes in O2, NPP & Ra
+0.15 - -0.08 # GPP
+SankeyR(input = 6.39, losses = c(0.15, 0.08, 6.16),
+        labels = c("GPP", "Ra", "Re", "NPP"),
+        unit = "mg O2/L", format = "pdf")
+
+SankeyR(input = 0.23, losses = c(0.15, 0.08),
+        labels = c("GPP", "Ra", "NPP"),
+        unit = "mg O2/L", format = "pdf")
+
+SankeyR(input = 0.23, losses = c(0.08, 0.15), # C is right
+        labels = c("GPP", "Ra", "NPP"),
+        unit = "mg O2/L", format = "pdf")
+
+SankeyR(input = c(6.24, 0.15), losses = c(0.08, 6.31),
+        labels = c("Dissolved O2", "GPP", "Ra", "NPP"),
+        unit = "mg O2/L", format = "pdf")
